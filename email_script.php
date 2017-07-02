67973ae2a060366ab8839ee848dbcd1db5774a44
Vaughn-Nichols <vaughn_nichols@hotmail.com>
2017-07-02 08:52:23 -0400

    update files

--- email_script.php
+++ email_script.php
@@ -5,12 +5,12 @@
 
 <?php
 
-$for="vaughn_nichols@hotmail.com";
+$from="vaughn_nichols@hotmail.com";
 $email=$_POST['email'];
 $subject=$_POST['subject'];
 $message=$_POST['message'];
 
-mail ($email, $subject, $message, "From:".$for);
+mail ($email, $subject, $message, "From:".$from);
 
 print "your message has been sent to: </br>$email</br>$subject</br>$message</p>";
 
